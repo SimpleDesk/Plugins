be91a66e22a76f4abd0cbbda9681bdbce1ce785b
gruffen <gruffen@simpledesk.net>
2011-08-21 22:05:20 +0000

    ! [Report to Helpdesk plugin] Ensured there are US + UK language files, and in both ISO and UTF-8 encoding.

---
+++ report_to_helpdesk/SDPluginReportToHelpdesk.english_british-utf8.php
@@ -0,0 +1,39 @@
+<?php
+###############################################################################
+#           Simple Desk Project - www.simpledesk.net                          #
+###############################################################################
+#         An advanced help desk modifcation built on SMF                      #
+###############################################################################
+#                                                                             #
+#         * Copyright 2010 - SimpleDesk.net                                   #
+#                                                                             #
+#   This file and its contents are subject to the license                     #
+#   included with this distribution, license.txt, which                       #
+#   states that this software is New BSD Licensed.                            #
+#   Any questions, please contact SimpleDesk.net                              #
+#                                                                             #
+###############################################################################
+# SimpleDesk Version: 2.0 Anatidae                                            #
+# File Info: SDPluginReportToHelpdesk.english_british-utf8.php / 2.0 Anatidae #
+###############################################################################
+// Version: 2.0 Anatidae; SimpleDesk staff list plugin
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+$txt['shdp_report_to_helpdesk'] = 'Report to Helpdesk';
+$txt['shdp_report_to_helpdesk_desc'] = 'This plugin redirects reported posts to the helpdesk to be dealt with.';
+
+$txt['reported_post'] = 'Reported post';
+$txt['reported_body'] = 'The following post, "{subject}" by {author}, was reported by {reporter}' . "\n" . '---------------------------------------' . "\n" . '{body}' . "\n" . '---------------------------------------' . "\n\n" . 'The reason given was: ' . "\n" . '{comment}';
+
+$txt['report_to_mod_func'] = 'Use this function to inform the moderators and administrators of an abusive or wrongly posted message.';
+
+$txt['report_normally'] = 'Do not direct reports to the helpdesk';
+$txt['report_posts_dept'] = 'Where should reported posts end up?';
+
+$txt['report_pms_dept'] = 'Where should reported private messages end up?';
+$txt['reported_pm'] = 'Reported private message';
+$txt['reported_pm_body_no_extra'] = 'The following message, "{subject}" by {author}, was reported by {reporter}' . "\n" . '---------------------------------------' . "\n" . '{body}' . "\n" . '---------------------------------------' . "\n\n" . 'The reason given was: ' . "\n" . '{comment}';
+$txt['reported_pm_body_extra'] = 'The following message, "{subject}" by {author}, was reported by {reporter}, and had also been sent to: {recipients}' . "\n" . '---------------------------------------' . "\n" . '{body}' . "\n" . '---------------------------------------' . "\n\n" . 'The reason given was: ' . "\n" . '{comment}';
+
+?>
